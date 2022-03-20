5f6aeed984f595cc29ed77ca4748043bd54a1997
PremPk57 <premgi2020@gmail.com>
2022-03-20 17:07:33 +0530

    sample change

---
+++ Student_Management_System/src/main/java/com/hcl/Student_Management_System/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hcl.Student_Management_System;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StudentManagementSystemApplication.class);
+	}
+
+}
---
+++ Student_Management_System/src/main/java/com/hcl/Student_Management_System/StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.Student_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+}
